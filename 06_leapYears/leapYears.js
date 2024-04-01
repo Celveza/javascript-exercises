1777a6e1088c31e8c1161c5b5bb85e7c1e2a9054
celveza <adrielip2007@gmail.com>
2024-04-02 00:42:49 +0200

    Exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            return year % 400 === 0;
+        }
+        else { return true; }
+    }
+    else { return false; }
 };
-
+console.log(leapYears(1996))
 // Do not edit below this line
 module.exports = leapYears;
