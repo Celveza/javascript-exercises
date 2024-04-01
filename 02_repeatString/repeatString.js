df9511b73fe8953fbc6176099d3dfc95a7d4b95c
Celveza <adrielip2007@gmail.com>
2024-04-01 20:45:53 +0200

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function( word, repeat ) {
+    let repeated = "";
+    for (let i = 0; i < repeat; ++i) {
+       repeated += word;
+    }
+    if (repeat < 0){
+        return "ERROR";
+    }
+    return repeated;
 };
-
+console.log(repeatString("hey",0))
 // Do not edit below this line
 module.exports = repeatString;
