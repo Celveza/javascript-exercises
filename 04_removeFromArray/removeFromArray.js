62a3045b2655e8c72d1490e7b3927e989cc6176b
celveza <adrielip2007@gmail.com>
2024-04-01 22:59:26 +0200

    Exercise 4 completed :D (It was a tough one)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...para) {
+    return array.filter(item => !para.includes(item))
 };
 
 // Do not edit below this line
