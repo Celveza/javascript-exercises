aa2f5f43d56ca629d2283226c166709e0f269a64
celveza <adrielip2007@gmail.com>
2024-04-02 01:00:28 +0200

    Exercise 7 completed and finished arrays and loops part

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsius = (fahr - 32) * (5/9);
+  if (celsius === 0){
+    return celsius;
+  }
+
+  else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahr = celsius * (9/5) + 32;
+
+  if (fahr === 0) {
+    return fahr;
+  }
+  else {
+    return parseFloat(fahr.toFixed(1));
+  }
 };
 
+console.log(convertToCelsius(100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
